bcb9f46c1a2b7765a07f7df59cc5b11aef923c7d
AleksYuan <leshajanaev@yandex.ru>
2023-03-21 16:06:16 +0300

    исправил на рестконтроллер

--- src/main/java/com/example/WalletProject/controllers/TransferController.java
+++ src/main/java/com/example/WalletProject/controllers/TransferController.java
@@ -1,7 +1,9 @@
 package com.example.WalletProject.controllers;
 
 import com.example.WalletProject.services.TransferService;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TransferController {
     private final TransferService transferService;
 
