a981ae2fee6039a5f9f8dda8706b57f9d6ec035e
AleksYuan <leshajanaev@yandex.ru>
2023-03-23 15:04:24 +0300

    контроллер транзакций

---
+++ src/main/java/com/example/WalletProject/controllers/TransactionController.java
@@ -0,0 +1,31 @@
+package com.example.WalletProject.controllers;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping("/{id}")
+    public TransactionForClientDTO getTransactionDtoForClientById(@PathVariable("id") Long transactionId) {
+        return transactionService.getTransactionDtoForClientById(transactionId);
+    }
+
+    @PatchMapping("/{id}")
+    public TransactionForClientDTO updateTransactionById(@PathVariable("id") Long transactionId,
+            @Valid @RequestBody TransactionForClientDTO transactionForClientDTO) {
+        return transactionService.updateTransactionFromClientById(transactionId, transactionForClientDTO);
+    }
+
+    @PostMapping("/new")
+    public TransactionForClientDTO saveNewTransaction(@Valid @RequestBody TransactionForClientDTO transactionForClientDTO) {
+        return transactionService.saveNewTransactionFromClientInRepo(transactionForClientDTO);
+    }
+}
