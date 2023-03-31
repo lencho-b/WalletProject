e1c2ed26ea8b45035b30c02294cbe5e9ea4b54b5
lesha <leshajanaev@yandex.ru>
2023-03-31 14:33:25 +0300

    flyaway + docker-compose

--- src/main/resources/db/migration/V2__insert_date_to_db.sql
+++ src/main/resources/db/migration/V2__insert_date_to_db.sql
@@ -1,28 +1,28 @@
-insert into client(firstname, lastname, patronymic, date_of_birth, email, phone_number, created_at, updated_at, frozen,
-                   is_delete, is_verify)
-values ('igor', 'sobol', 'evg', '22.03.1908', 'gocha@lalalal.ru', '8002353535', '20.03.2000', '20.03.2000', false,
-        false, false);
-
-insert into currency(name)
-values ('rub');
-
-insert into account(name, frozen, comment, value, created_at, updated_at, client_id, currency_id)
-values ('first', false, 'test1', 10000, '20.03.2000', '20.03.2000', 1, 1),
-       ('second', false, 'test2', 10000, '20.01.2000', '20.01.2000', 1, 1),
-       ('third', false, 'test3', 10000, '20.01.2000', '20.01.2000', 1, 1);
-
-insert into transaction_type(type, comment) values ('перевод', 'отправление денег с активного счета'),
-                                                   ('получение', 'получение денег на активный счет'),
-                                                   ('оплата','проведение платежа с активного счета');
-
-insert into transaction(value, message, start_date_time, finish_date_time, status, type_id)
-VALUES (100, 'перевод тест1 ', now(), null, false, 1);
-
-
-insert into transaction_account(sender, account_id, transaction_id)
-values (true, 1, 1),
-       (false, 2, 1);
-
-insert into role(name)
-values ('user'),
-       ('admin');
+-- insert into client(firstname, lastname, patronymic, date_of_birth, email, phone_number, created_at, updated_at, frozen,
+--                    is_delete, is_verify)
+-- values ('igor', 'sobol', 'evg', '22.03.1908', 'gocha@lalalal.ru', '8002353535', '20.03.2000', '20.03.2000', false,
+--         false, false);
+--
+-- insert into currency(name)
+-- values ('rub');
+--
+-- insert into account(name, frozen, comment, value, created_at, updated_at, client_id, currency_id)
+-- values ('first', false, 'test1', 10000, '20.03.2000', '20.03.2000', 1, 1),
+--        ('second', false, 'test2', 10000, '20.01.2000', '20.01.2000', 1, 1),
+--        ('third', false, 'test3', 10000, '20.01.2000', '20.01.2000', 1, 1);
+--
+-- insert into transaction_type(type, comment) values ('перевод', 'отправление денег с активного счета'),
+--                                                    ('получение', 'получение денег на активный счет'),
+--                                                    ('оплата','проведение платежа с активного счета');
+--
+-- insert into transaction(value, message, start_date_time, finish_date_time, status, type_id)
+-- VALUES (100, 'перевод тест1 ', now(), null, false, 1);
+--
+--
+-- insert into transaction_account(sender, account_id, transaction_id)
+-- values (true, 1, 1),
+--        (false, 2, 1);
+--
+-- insert into role(name)
+-- values ('user'),
+--        ('admin');
