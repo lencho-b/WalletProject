45dc728b3f7df1920f59eb805031b2cd2b22921f
Emilia2022 <106738248+Emilia2022@users.noreply.github.com>
2023-04-07 16:59:12 +0300

    testClientController

---
+++ src/test/java/com/example/WalletProject/controller/ClientControllerTest.java
@@ -0,0 +1,72 @@
+package com.example.WalletProject.controller;
+
+import com.example.WalletProject.services.AccountService;
+import com.example.WalletProject.services.ClientService;
+import com.example.WalletProject.services.DocumentService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ClientControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    ClientService clientService;
+    @MockBean
+    AccountService accountService;
+    @MockBean
+    DocumentService documentService;
+
+
+    @WithMockUser(value = "spring")
+    @Test
+    public void showClientById() throws Exception {
+        mockMvc.perform(get("/client/1/information").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    @WithMockUser(value = "spring")
+    @Test
+    public void showClientInformationForManageByClientId() throws Exception {
+        mockMvc.perform(get("/client/1/manage-information").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    public void showAllAccountsByClientId() throws Exception {
+        mockMvc.perform(get("/client/1/account").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+    @WithMockUser(value = "spring")
+    @Test
+    public void showClientsAccountById() throws Exception {
+        mockMvc.perform(get("/client/1/account/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    public void showDocumentByClientId() throws Exception {
+        mockMvc.perform(get("/client/1/document").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+
+
+
+}
